1eeffd11013f5896eef0ddb5966f5b89f6e69a7d
Hikmatillo <98952821+tillo2002@users.noreply.github.com>
2022-11-17 20:00:09 +0300

    Add files via upload

---
+++ dist/precache-manifest.946df7453b0445687f849900c64c25d3.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87021d3d54b8b5b01187",
+    "url": "/css/app.37adbbe3.css"
+  },
+  {
+    "revision": "1bdcaf335d98ad06bcb30d04b65a80d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "87021d3d54b8b5b01187",
+    "url": "/js/app.524e5e5b.js"
+  },
+  {
+    "revision": "b8342a7d5898c618c90f",
+    "url": "/js/chunk-2d0a4783.a418688e.js"
+  },
+  {
+    "revision": "7716869106e8b6273812",
+    "url": "/js/chunk-2d0be70b.33eb7d66.js"
+  },
+  {
+    "revision": "5a8ca0be2bae3f86581c",
+    "url": "/js/chunk-2d216dc7.9d2d6d73.js"
+  },
+  {
+    "revision": "1f71938cce1ba2c3ad88",
+    "url": "/js/chunk-vendors.dd3b3169.js"
+  },
+  {
+    "revision": "f11bce428d38fd9f4d6f0ba31c2acffc",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
